debe5e488595db75a9992ecf0c4d32165a9f7400
Malte Kliemann <mail@maltekliemann.com>
2023-03-11 22:22:21 +0100

    Remove unnecessary print

--- usdc.py
+++ usdc.py
@@ -8,6 +8,5 @@ kraken = pykrakenapi.KrakenAPI(krakenex.API())
 time = datetime(2023, 4, 1, 0, 0, tzinfo=pytz.utc)
 timestamp = datetime.timestamp(time)
 data_frame, _ = kraken.get_ohlc_data("USDCUSD", interval=60 * 24, since=timestamp - 1)
-print(data_frame)
 result = data_frame.loc[data_frame["time"] == timestamp]["close"]
 print(result)
